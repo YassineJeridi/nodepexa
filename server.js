89aaf6e4f3824d54a8449237c7717d0f767ca1de
PeXxa <yassinejeridi23@gmail.com>
2025-03-05 08:08:48 +0100

    'CRUD'

--- server.js
+++ server.js
@@ -1,31 +1,187 @@
 const express = require('express');
-
+const User = require('./models/user');
 require('./config/connect');
 
 const app = express();
+app.use(express.json());
 
-app.post('/add',()=>{
+app.post('/add',(req,res)=>{
 
-    console.log('add work');
+    data = req.body;
+    usr = new User(data);
+    usr.save()
+        .then(
+            (savedUser)=>{
+                res.send(savedUser)
+            }
+        )
+        .catch(
+            (err)=>{
+                res.send(err)
+            }
+        )
+
+
+    console.log('user added');
 
 });
 
-app.get('/getall' , ()=>{
+app.post('/create', async (req,res)=>{
+    try {
+        data = req.body;
+        usr = new User(data);
 
-    console.log('get work');
+        savedUser = await usr.save();
+
+        res.send(savedUser)
+
+
+    } catch (error) {
+        res.send(error)
+        
+    }
 
 })
 
-app.put('/update' , ()=>{
 
-    console.log('update work');
+
+app.get('/getall' , (req,res)=>{
+    User.find()
+    .then(
+        (users)=>{
+        res.send(users);
+    })
+    .catch(
+        (err)=>{
+        res.send(err)
+    })
 
 })
-app.delete('/delete',()=>{
 
-    console.log('delete work');
-    
+
+
+
+
+app.get('/all',async(req,res)=>{
+    try {
+        users = await User.find();
+        res.send(users);
+
+        
+        
+    } catch (error) {
+        res.send(error)
+        
+    }
 })
+
+app.get('/filter',async(req,res)=>{
+    try {
+        users = await User.find( {age:21  ,name:"ela"} );
+        res.send(users);
+
+        
+        
+    } catch (error) {
+        res.send(error)
+        
+    }
+})
+
+
+
+
+app.get("/findbyid/:id" ,(req,res)=>{
+    myid = req.params.id;
+    User.findById(myid)
+    .then(
+        (user)=>{
+        res.send(user)
+    })
+    .catch(
+        (err)=>{
+        res.send(err)
+
+    })
+})
+
+
+
+app.get('/findone/:id',async(req,res)=>{
+    try {
+        myid = req.params.id;
+        users = await User.findOne({_id : myid})
+        res.send(users);
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+
+
+app.delete('/delete/:id',(req,res)=>{
+
+    myid = req.params.id;
+    User.findOneAndDelete({_id:myid})
+    .then(
+        (deletedUser)=>{
+            res.send(deletedUser);
+        }
+    )
+    .catch(        
+        (err)=>{
+        res.send(err)
+            
+    })
+})
+
+
+app.delete('/delete2/:id',async (req ,res)=>{
+    try {
+        myid = req.params.id;
+        deleteduser = await User.findByIdAndDelete(myid);
+        res.send(deleteduser);
+        
+    } catch (error) {
+        res.send(error);        
+    }
+}) 
+
+
+app.put('/update/:id' , (req,res)=>{
+    myid = req.params.id;
+    data = req.body;
+    User.findOneAndUpdate({_id:myid},data)
+    .then(
+        (updateduser)=>{
+            res.send(updateduser);
+
+        }
+    )
+    .catch(
+        (err)=>{
+            res.send(err)
+
+    }
+)
+})
+
+
+app.put('/update2/:id',async(req,res)=>{
+    try {
+        myid = req.params.id;
+        data = req.body;
+        update = await User.findByIdAndUpdate({_id:myid},data);
+        res.send(update);
+
+
+    } catch (error) {
+        res.send(error)
+        
+    }
+})
+
+
 
 
 app.listen(3000,()=>{
